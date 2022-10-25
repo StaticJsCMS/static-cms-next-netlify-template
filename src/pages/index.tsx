bbde20540e9b9de273530ea43341a1e7bc60a116
Daniel Lautzenheiser <lautzd@gmail.com>
2022-10-25 19:29:08 -0400

    Add netlify identify widget

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Layout from "../components/Layout";
 import BasicMeta from "../components/meta/BasicMeta";
 import OpenGraphMeta from "../components/meta/OpenGraphMeta";
@@ -6,57 +7,65 @@ import { SocialList } from "../components/SocialList";
 
 export default function Index() {
   return (
-    <Layout>
-      <BasicMeta url={"/"} />
-      <OpenGraphMeta url={"/"} />
-      <TwitterCardMeta url={"/"} />
-      <div className="container">
-        <div>
-          <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
-          </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
-          <SocialList />
+    <>
+      <Head>
+        <script
+          src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          async
+        />
+      </Head>
+      <Layout>
+        <BasicMeta url={"/"} />
+        <OpenGraphMeta url={"/"} />
+        <TwitterCardMeta url={"/"} />
+        <div className="container">
+          <div>
+            <h1>
+              Hi, We're Next.js & Static CMS<span className="fancy">.</span>
+            </h1>
+            <span className="handle">@nextjs-static-cms-blog</span>
+            <h2>A blog template with Next.js and Static CMS.</h2>
+            <SocialList />
+          </div>
         </div>
-      </div>
-      <style jsx>{`
-        .container {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex: 1 1 auto;
-          padding: 0 1.5rem;
-        }
-        h1 {
-          font-size: 2.5rem;
-          margin: 0;
-          font-weight: 500;
-        }
-        h2 {
-          font-size: 1.75rem;
-          font-weight: 400;
-          line-height: 1.25;
-        }
-        .fancy {
-          color: #15847d;
-        }
-        .handle {
-          display: inline-block;
-          margin-top: 0.275em;
-          color: #9b9b9b;
-          letter-spacing: 0.05em;
-        }
-
-        @media (min-width: 769px) {
+        <style jsx>{`
+          .container {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex: 1 1 auto;
+            padding: 0 1.5rem;
+          }
           h1 {
-            font-size: 3rem;
+            font-size: 2.5rem;
+            margin: 0;
+            font-weight: 500;
           }
           h2 {
-            font-size: 2.25rem;
+            font-size: 1.75rem;
+            font-weight: 400;
+            line-height: 1.25;
           }
-        }
-      `}</style>
-    </Layout>
+          .fancy {
+            color: #15847d;
+          }
+          .handle {
+            display: inline-block;
+            margin-top: 0.275em;
+            color: #9b9b9b;
+            letter-spacing: 0.05em;
+          }
+
+          @media (min-width: 769px) {
+            h1 {
+              font-size: 3rem;
+            }
+            h2 {
+              font-size: 2.25rem;
+            }
+          }
+        `}</style>
+      </Layout>
+    </>
   );
 }
