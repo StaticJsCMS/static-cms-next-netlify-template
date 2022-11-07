5c50e1c4bb145ab441b4d617373372ad432c598c
Daniel Lautzenheiser <lautzd@gmail.com>
2022-11-07 16:44:13 -0500

    WIP

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,3 +1,16 @@
-export default function Index() {
-  return <div>hello!</div>;
-}
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+
+const Admin = () => {
+  const CMSPage = useMemo(
+    () =>
+      dynamic(() => import("../components/cms/CMSPage"), {
+        ssr: false,
+      }),
+    []
+  );
+
+  return useMemo(() => <CMSPage key="admin" />, [CMSPage]);
+};
+
+export default Admin;
