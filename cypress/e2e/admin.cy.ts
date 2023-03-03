403203f7f1974b27c621ae121700a58abe85709b
Daniel Lautzenheiser <lautzd@gmail.com>
2023-03-03 13:43:54 -0500

    Update admin.cy.ts

--- cypress/e2e/admin.cy.ts
+++ cypress/e2e/admin.cy.ts
@@ -3,6 +3,6 @@ export {};
 describe("admin page", () => {
   it("loads", () => {
     cy.visit("/admin");
-    cy.contains("button", "Login with Netlify Identity4").should("be.visible");
+    cy.contains("button", "Login with Netlify Identity").should("be.visible");
   });
 });
