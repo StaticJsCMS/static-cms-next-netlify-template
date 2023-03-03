e7288762ca6bcfb13fc0b7a943dfc76cb82fe81d
Daniel Lautzenheiser <lautzd@gmail.com>
2023-03-03 12:45:30 -0500

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
 
   e2e: {
     baseUrl: "http://localhost:3000",
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
